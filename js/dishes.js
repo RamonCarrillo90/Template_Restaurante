d3f6e20c7676c41dbbec98b7a9c577e49c400e80
RamonCarrillo90 <carrilloriverar70@gmail.com>
2026-08-01 01:47:49 -0700

    primer modelo

--- js/dishes.js
+++ js/dishes.js
@@ -36,7 +36,7 @@ const dishes = [
     price: "$290",
     emoji: "🌮",
     image: "",
-    modelSrc: "",
+    modelSrc: "assets/models/Astronaut.glb",
     info: "Porción de 2 tacos. Diámetro de tortilla: 12cm. Presentado en comal de barro sobre la mesa. Contiene gluten."
   },
   {
